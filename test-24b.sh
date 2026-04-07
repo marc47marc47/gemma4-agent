e0de24d55348b5aea7655918eb10c75971f796c4
Marc Xiao <marc47marc47@gmail.com>
2026-04-07 11:47:31 +0800

    Add 24B model test script

---
+++ test-24b.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#你現在可以這樣測這顆模型：
+#npm run dev --                --model-source bartowski --model-repo bartowski/google_gemma-4-26B-A4B-it-GGUF --model-file google_gemma-4-26B-A4B-it-IQ2_XXS.gguf
+
+#如果只想先下載不啟動：
+npm run dev -- --download-only --model-source bartowski --model-repo bartowski/google_gemma-4-26B-A4B-it-GGUF --model-file google_gemma-4-26B-A4B-it-IQ2_XXS.gguf
+
